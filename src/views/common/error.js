1e3ad74ad00df918338138a767ac9ae0d500a67d
Sankar <gaurisankarj@gmail.com>
2019-04-28 02:12:34 +0530

    Prototype 1.1.0

---
+++ src/views/common/error.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Error = () => (
+    <div>
+        <div id="error">
+            <h1>Error!</h1>
+            <p>Sorry. Your request was unsuccessful.</p>
+        </div>
+    </div>
+);
+
+
+export default Error;
